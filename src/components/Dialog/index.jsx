004d91d21bda3dcf111f72882257780b73b973f0
guilhercs <guilherme.carvalhos@outlook.com>
2025-12-05 16:06:47 -0300

    feat: add dialog component and useRef hook

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import "./dialog.style.css";
 
 export function Dialog() {
+  //O UseRef serve para criar uma referência mutável que persiste por todo o ciclo de vida do componente. Ele é comumente usado para acessar diretamente elementos DOM ou armazenar valores mutáveis que não causam uma nova renderização quando alterados. De forma simples, ele permite que você mantenha uma referência a um elemento ou valor entre renderizações sem disparar uma atualização do componente. Alternativa ao document.getElementById ou outras manipulações diretas do DOM (pois o document.getElementById retorna null).
   const dialog = useRef();
 
   const openDialog = () => {
